e4b642eaabea29d25986ba694bec228ed9aaf316
User <User@User-PC>
2016-08-16 14:41:53 +0900

    add user input from console

--- src/kr/co/kosta/passvalidator/PassValidatorMain.java
+++ src/kr/co/kosta/passvalidator/PassValidatorMain.java
@@ -3,11 +3,19 @@ package kr.co.kosta.passvalidator;
 import kr.co.kosta.passvalidator.service.Validator;
 import static kr.co.kosta.passvalidator.util.PasswordComplexityContants.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class PassValidatorMain {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		String passwd = "123445675%";
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+		
+		String passwd = br.readLine();
+		System.out.print("user Input>");
+		
 		
 		Validator validator = new Validator();
 		
